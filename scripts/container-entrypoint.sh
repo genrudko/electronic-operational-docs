74e9e99c76129a244adc4470fd3fd7c5306ebf0f
genrudko <54811999+genrudko@users.noreply.github.com>
2026-07-24 16:43:36 +0300

    Allow quiet one-off preview commands

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -eu
 
-python manage.py check
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
+if [ "${EOD_SKIP_STARTUP_TASKS:-0}" != "1" ]; then
+    python manage.py check
+    python manage.py migrate --noinput
+    python manage.py collectstatic --noinput --clear
+fi
 
 exec "$@"
