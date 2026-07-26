61caa69ece7ad99a12c2f064488e96d65fd0a0b6
genrudko <54811999+genrudko@users.noreply.github.com>
2026-07-26 19:55:08 +0300

    DEFECT-001: configure equipment defects app

---
+++ src/apps/equipment_defects/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EquipmentDefectsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.equipment_defects"
+    verbose_name = "Журнал дефектов оборудования"
